6538ec40b8d0e26de90346859dd383ee82cac111
DevCarlos <maiscarlos@gmail.com>
2022-04-06 13:54:34 -0300

    Projeto Criado

---
+++ src/main/java/com/dev/projectmc/ProjectmcApplication.java
@@ -0,0 +1,13 @@
+package com.dev.projectmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/projectmc/ProjectmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.projectmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

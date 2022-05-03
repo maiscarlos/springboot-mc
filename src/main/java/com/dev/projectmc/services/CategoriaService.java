867b99589659cbca0bbee9501469dca7dc398b89
DevCarlos <maiscarlos@gmail.com>
2022-05-03 15:02:28 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/dev/projectmc/services/CategoriaService.java
+++ src/main/java/com/dev/projectmc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.dev.projectmc.services;
 
 import com.dev.projectmc.domain.Categoria;
 import com.dev.projectmc.repositories.CategoriaRepository;
+import com.dev.projectmc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,8 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id){
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
     }
 
